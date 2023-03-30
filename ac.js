1f899424a94bc85b830f330f8ceb3e61344cb264
im4uajnds <53973675+im4uajnds@users.noreply.github.com>
2023-03-30 13:26:55 +0530

    Add files via upload

---
+++ ac.js
@@ -0,0 +1,45 @@
+Status = "";
+objects=[];
+AC_image = "";
+
+function preload(){
+    AC_image = loadImage("AC.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(AC_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects=results;
+}
+
+function draw(){
+    if(Status!=""){
+        image(AC_image,0,0,640,350);
+        for(var i= 0; i<Objects.length; i++){
+            document.getElementById("Status").innerHTML="Status: objects detected";
+            fill("#FF0000");
+            percent=floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+
+
+}
---
+++ light.js
@@ -0,0 +1,43 @@
+Status = "";
+LightBulb_image = "";
+objects=[];
+
+function preload(){
+    LightBulb_image = loadImage("Light Bulb.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(LightBulb_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects=results;
+}
+
+function draw(){
+    if(Status!=""){
+        image(LightBulb_image,0,0,640,350);
+        for(var i= 0; i<Objects.length; i++){
+            document.getElementById("Status").innerHTML="Status: objects detected";
+            fill("#FF0000");
+            percent=floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
